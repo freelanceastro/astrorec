a4dac0829c09b9a32f0f96467f8517e26fbf5649
Jonathan Sick <jonathansick@mac.com>
2014-12-09 23:15:08 -0600

    Get rich citations from a local latex doc

--- astrorec/latexrec.py
+++ astrorec/latexrec.py
@@ -4,9 +4,18 @@
 Recommender based on a LaTeX file.
 """
 
+from paperweight.document import FilesystemTexDocument
+
 
 class LaTeXRecommender(object):
     """Recommend papers to be cited in your LaTeX file."""
     def __init__(self, tex_filepath):
         super(LaTeXRecommender, self).__init__()
         self._filepath = tex_filepath
+        self._doc = FilesystemTexDocument(tex_filepath)
+        self._doc.inline_inputs()
+        self._doc.remove_comments()
+        rich_cites = self._doc.rich_bib_keys
+        print rich_cites
+        print type(rich_cites)
+        # self._doc.bibtex_path  # we're using bibtex, right?
